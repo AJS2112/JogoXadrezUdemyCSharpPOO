cb41195ce4e60d1e2f69cf46c851c1a6f4c143b6
Antonio Sucre <ajs2104@gmail.com>
2020-02-25 15:45:00 -0300

    Testes iniciais com possiveis movimentos das pecas

--- xadrez-console/xadrez/Torre.cs
+++ xadrez-console/xadrez/Torre.cs
@@ -5,12 +5,77 @@ namespace xadrez
     {
         public Torre(Cor cor, Tabuleiro tab) : base(cor,tab)
         {
-
         }
 
         public override string ToString()
         {
             return "T";
+        }
+
+        private bool PodeMover(Posicao pos)
+        {
+            Peca p = tab.peca(pos);
+            return p == null || p.cor != cor;
+        }
+
+        public override bool[,] MovimentosPossiveis()
+        {
+            bool[,] mat = new bool[tab.linhas, tab.colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            //acima
+            pos.DefinirValores(posicao.linha - 1, posicao.coluna);
+            while(tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos)!=null && tab.peca(pos).cor != this.cor)
+                {
+                    break;
+                }
+                pos.linha -= 1;
+            }
+
+            //abaixo
+            pos.DefinirValores(posicao.linha + 1, posicao.coluna);
+            while (tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != this.cor)
+                {
+                    break;
+                }
+                pos.linha += 1;
+            }
+
+
+            //direita
+            pos.DefinirValores(posicao.linha, posicao.coluna + 1);
+            while (tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != this.cor)
+                {
+                    break;
+                }
+                pos.coluna += 1;
+            }
+
+            // esquerda
+            pos.DefinirValores(posicao.linha, posicao.coluna-1);
+            while (tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != this.cor)
+                {
+                    break;
+                }
+                pos.coluna -= 1;
+            }
+
+            return mat;
+
+
         }
     }
 }
